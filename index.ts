cec6e15a16e9cf26ae371a935c61f0f72d1516af
diefbell <diefenbaker.bell@gmail.com>
2024-08-21 23:31:26 +0100

    Eh it's getting there maybe

--- index.ts
+++ index.ts
@@ -15,6 +15,19 @@ const typeChecker = program.getTypeChecker();
 
 const sourceFile = program.getSourceFile("test.ts")!;
 
+
+// const findAddSymbol = (node: ts.Node, name: string): ts.Symbol | undefined =>
+// {
+// 	if (ts.isIdentifier(node))
+// 	{
+// 		const symbol = checker.getSymbolAtLocation(node);
+// 		if (symbol && symbol.name === name)
+// 		{
+// 			return symbol;
+// 		}
+// 	}
+
+
 const findAddMember = (node: ts.Node): ts.PropertyDeclaration | null => {
 	if(ts.isClassDeclaration(node)) {
 		for(const child of node.members.filter(ts.isPropertyDeclaration)) {
@@ -97,14 +110,22 @@ const getConstructorType = (identifier: Identifier): ts.Type | null => {
  * @param node 
  * @returns 
  */
+
+// DIEF: MAKE THIS ALSO RETURN WHETHER OR NOT IT GOT UPDATED!
 const updateNode = <T extends ts.Node>(node: T): T => {
 	if(!node) {
 		return node;
 	}
 
+	const isUpdated = (node: ts.Node): boolean => Object.hasOwn(node, "original");
+
 	if(ts.isSourceFile(node)) {
+		// console.log("Source file!");
 		const updatedStatements = node.statements.map(updateNode);
-		return ts.factory.updateSourceFile(node, updatedStatements) as any as T;
+		node.fileName = node.fileName.replace(".ts", ".updated.ts");
+		const updatedSourceFile = ts.factory.updateSourceFile(node, updatedStatements) as any as T;
+		// console.log(updatedSourceFile.getText());
+		return updatedSourceFile;
 	}
 
 	if(ts.isVariableStatement(node)) {
@@ -117,7 +138,15 @@ const updateNode = <T extends ts.Node>(node: T): T => {
 
 	if(ts.isVariableDeclarationList(node)) {
 		// console.log("VariableDeclarationList: ", node.getText());
-		const updatedDeclarations = node.declarations.map(updateNode);
+		const declarations = node.declarations.map(updateNode);
+		const updatedDeclarations = declarations.filter(isUpdated);
+
+		if(updatedDeclarations.length === 0) {
+			return node;
+		}
+
+		console.log("DECS", node.getText());
+
 		return ts.factory.updateVariableDeclarationList(node, updatedDeclarations) as any as T;
 	}
 
@@ -125,22 +154,23 @@ const updateNode = <T extends ts.Node>(node: T): T => {
 		// console.log("VariableDeclaration: ", node.getText());
 		const updatedInitializer = !!node.initializer
 			&& ts.isBinaryExpression(node.initializer)
-			&& updateNode(node.initializer);
-		
+			&& updateNode<ts.Expression>(node.initializer);
+
 		if(!updatedInitializer) {
 			return node;
 		}
 
-		return ts.factory.updateVariableDeclaration(node,
+		const updatedVariableDelcaration = ts.factory.updateVariableDeclaration(node,
 			node.name,
 			node.exclamationToken,
 			node.type,
 			updatedInitializer
-		) as any as T;
+		);
+		
+		return updatedVariableDelcaration as any as T;
 	}
 
 	if(ts.isBinaryExpression(node)) {
-		console.log("BinaryExpression: ", node.getText(), "\n");
 		if(!ts.isPlusToken(node.operatorToken)) {
 			return node;
 		}
@@ -158,309 +188,212 @@ const updateNode = <T extends ts.Node>(node: T): T => {
 		if(!leftConstructorType) {
 			return node;
 		}
-		console.log(typeChecker.isTypeAssignableTo(leftConstructorType, addOperatorOverload.leftType));
+
+		const isConstructorFor = (t1: ts.Type, t2: ts.Type): boolean => 
+			typeChecker.isTypeAssignableTo(t1, t2) && typeChecker.isTypeAssignableTo(t2, t1);
+
+		if(!isConstructorFor(leftConstructorType, addOperatorOverload.leftType)) {
+			return node;
+		}
+
+		console.log("HERE A")
 
 		const rightConstructorType = getConstructorType(right);
 		if(!rightConstructorType) {
 			return node;
 		}
 
-		console.log(typeChecker.isTypeAssignableTo(rightConstructorType, addOperatorOverload.rightType));
-
-
-
-
-
-
-
-		const rightSymbol = typeChecker.getSymbolAtLocation(right);
-		if(!rightSymbol) {
+		if(!isConstructorFor(rightConstructorType, addOperatorOverload.rightType)) {
 			return node;
 		}
-		const rightType = typeChecker.getTypeOfSymbolAtLocation(rightSymbol, right);
 
+		console.log("HERE B")
 
-
-		// console.log(
-		// 	// leftClassType === addOperatorOverload.leftType
-		// 	typeChecker.isTypeAssignableTo(leftType, addOperatorOverload.leftType)
-		// );
-		// console.log(
-		// 	typeChecker.isTypeAssignableTo(rightType, addOperatorOverload.rightType)
-		// );
-		
-		// const rightTypeSymbol = checker.getTypeAtLocation(node.right).getSymbol();
-
-		// console.log(leftTypeSymbol)
+		return ts.factory.createCallExpression(addOperatorOverload.func, [], [left, right]) as any as T;
 	}
 
 	return node;
 }
 
-updateNode(sourceFile);
+const updatedSourceFile = updateNode(sourceFile);
 
-// const findSymbol = (node: ts.Node, name: string): ts.Symbol | undefined =>
-// {
-// 	if (ts.isIdentifier(node))
-// 	{
-// 		const symbol = checker.getSymbolAtLocation(node);
-// 		if (symbol && symbol.name === name)
-// 		{
-// 			return symbol;
+const s = updatedSourceFile.statements[3];
+console.log(s.getText(updatedSourceFile));
+
+
+
+
+
+
+
+
+
+
+const virtualFiles = new Map<string, ts.SourceFile>();
+virtualFiles.set(updatedSourceFile.fileName, updatedSourceFile);
+
+const compilerOptions: ts.CompilerOptions = {
+	// Your desired compiler options
+	target: ts.ScriptTarget.ESNext,
+	module: ts.ModuleKind.CommonJS,
+};
+
+console.log("CWD", process.cwd());
+
+const customCompilerHost: ts.CompilerHost = {
+	getSourceFile: (fileName: string) =>
+	{
+		console.log("getSourceFile", fileName, virtualFiles.has(fileName));
+		return virtualFiles.get(fileName);
+	},
+	// Implement other methods as needed for your specific use case
+	writeFile: (fileName, data) => {
+		console.log("WRITING", data.toString());
+		fs.writeFileSync(fileName.replace(".ts", ".js"), data);
+	},
+	getDefaultLibFileName: () => 'lib.d.ts',
+	getCurrentDirectory: () => process.cwd(),
+	getCanonicalFileName: (fileName: string): string => fileName,
+	useCaseSensitiveFileNames: () => true,
+	getNewLine: (): string => '\n',
+	fileExists: (fileName: string): boolean =>
+	{
+		console.log("fileExists", fileName, virtualFiles.has(fileName));
+		return virtualFiles.has(fileName);
+	},
+	readFile: (fileName: string): string | undefined =>
+	{
+		console.log("NO SF");
+		const sf = virtualFiles.get(fileName);
+		console.log("NO SF");
+		if (sf)
+		{
+			console.log("A")
+			const sfContent = ts.createPrinter().printNode(ts.EmitHint.Unspecified, sf, sf);
+			console.log("B")
+			return sfContent;
+		}
+		return undefined;
+	},
+	// directoryExists: (directoryName: string): boolean =>
+	// {
+	// 	try
+	// 	{
+	// 		return fs.statSync(directoryName).isDirectory();
+	// 	} catch
+	// 	{
+	// 		return false;
+	// 	}
+	// },
+	// getDirectories: (directoryName: string): string[] =>
+	// {
+	// 	return fs.readdirSync(directoryName).filter(name => fs.statSync(path.join(directoryName, name)).isDirectory());
+
+	// },
+	// resolveModuleNames: (
+	// 	moduleNames: string[],
+	// 	containingFile: string,
+	// 	reusedNames?: string[],
+	// 	redirectedReferences?: ts.ResolvedProjectReference | undefined,
+	// 	options?
+	// ): (ts.ResolvedModule | undefined)[] =>
+	// {
+	// 	// Implement module resolution logic here
+	// 	return []
+	// },
+	// getDefaultLibLocation(): string
+	// {
+	// 	return 'lib.d.ts';
+	// }
+};
+
+const findBinaryExpression = (node: ts.Node): ts.Node | null => {
+	if(ts.isBinaryExpression(node)) {
+		return node;
+	}
+
+	if(ts.isSourceFile(node)) {
+		for(const statement of node.statements) {
+			const b = findBinaryExpression(statement);
+			if(b) {
+				return b
+			}
+		}
+	}
+
+	for(const child of node.getChildren()) {
+		const b = findBinaryExpression(child);
+		if(b) return b;
+	}
+
+	return null;
+}
+
+
+
+
+// const callExpressions: ts.CallExpression[] = [];
+// const findCallExpressions = (node: ts.Node) => {
+// 	if(ts.isSourceFile(node)) {
+// 		for(const statement of node.statements) {
+// 			findCallExpressions(statement);
 // 		}
 // 	}
 
-// 	const children = node.getChildren();
-// 	for (const child of children)
-// 	{
-// 		const s = findSymbol(child, name);
-// 		if (s) return s;
+// 	if(ts.isCallExpression(node)) {
+// 		callExpressions.push(node);
 // 	}
-// }
 
-// const findSymbolMatch = (node: ts.Node, matchSymbol: ts.Symbol): ts.Symbol | undefined =>
-// {
-// 	if (ts.isIdentifier(node))
-// 	{
-// 		const symbol = checker.getSymbolAtLocation(node);
-// 		if (symbol)
-// 		{
-// 			if (symbol=== matchSymbol)
-// 			{
-// 				return symbol;
-// 			}
+// 	try {
+// 		for(const child of node.getChildren()) {
+// 			findCallExpressions(child);
 // 		}
-// 	}
-
-// 	const children = node.getChildren();
-// 	for (const child of children)
-// 	{
-// 		const s = findSymbolMatch(child, matchSymbol);
-// 		if (s) return s;
-// 	}
+// 	} catch {}
 // }
 
-// const testSource = program.getSourceFile("test.ts");
+// findCallExpressions(updatedSourceFile);
+// console.log(callExpressions.length);
 
-// // const operatorsSource = program.getSourceFile("operators.ts")!;
-// const add = findSymbol(testSource!, "ADD");
-
-// const addOp = findSymbolMatch(testSource!, add!);
-// // console.log("addOp", addOp);
-
-// const findArray = (node: ts.Node): ts.ArrayLiteralExpression | undefined =>
-// {
-// 	if (ts.isArrayLiteralExpression(node))
-// 	{
-// 		return node;
-// 	}
-
-// 	const children = node.getChildren();
-// 	for (const child of children)
-// 	{
-// 		const a = findArray(child);
-// 		if (a) return a;
-// 	}
-// }
-
-// // console.log(findArray(testSource!));
-
-// const addMember = addOp?.getDeclarations()![0]
-// const arr = findArray(addMember!);
-// // console.log(arr);
-
-// const first = arr!.elements[0] as ts.ArrayLiteralExpression;
-// const func = first.elements[2] as ts.ArrowFunction;
-
-// let bins: ts.BinaryExpression[] = [];
-// const findBinaries = (node: ts.Node) =>
-// {
-// 	if (ts.isBinaryExpression(node))
-// 	{
-// 		bins.push(node);
-// 	}
-
-// 	ts.forEachChild(node, findBinaries);
-// }
-
-// findBinaries(testSource!);
-// const binExp = bins[bins.length - 1];
-// const lhs = checker.getTypeAtLocation(binExp.left);
-// const rhs = checker.getTypeAtLocation(binExp.right);
-// const call = ts.factory.createCallExpression(func, [], [binExp.left, binExp.right]);
-
-// const varDec = binExp.parent as ts.VariableDeclaration;
-
-// const newVarDec = ts.factory.updateVariableDeclaration(
-// 	varDec,
-// 	varDec.name,
-// 	varDec.exclamationToken,
-// 	varDec.type,
-// 	call
-// );
-
-// const varDecList = varDec.parent as ts.VariableDeclarationList;
-// const newVarDecList = ts.factory.updateVariableDeclarationList(
-// 	varDecList,
-// 	[newVarDec]
-// );
-
-// const varStatement = varDecList.parent as ts.VariableStatement;
-// const newVarStatement = ts.factory.updateVariableStatement(
-// 	varStatement,
-// 	undefined,
-// 	newVarDecList
-// );
+// const c = findCallExpression(updatedSourceFile);
+// console.log(c?.parent?.parent?.getText());
+// const bin = findBinaryExpression(updatedSourceFile);
 
 
+// const p = bin!.parent?.parent?.parent?.parent?.parent?.parent?.parent?.parent?.parent;
+// // @ts-ignore
+// delete p?.parent;
+// // @ts-ignore
+// delete p?.original;
+// console.log(p);
+// console.log(ts.isCallExpression(p));
+
+// cons
 
 
+process.exit(0);
 
+console.log(1, "UPDATED FILENAME", updatedSourceFile.fileName)
+const program2 = ts.createProgram([updatedSourceFile.fileName], compilerOptions, customCompilerHost);
 
+console.log("1b")
+// console.log(updatedSourceFile)
+console.log(2,
+	// virtualFiles.get(updatedSourceFile.fileName)
+)
+const emitResult = program2.emit(updatedSourceFile);
 
+console.log(3)
+console.log(emitResult);
 
-
-
-
-
-
-
-
-
-
-
-
-// // const sourceFile = varStatement.parent as ts.SourceFile;
-
-// const updatedSourceFile = ts.factory.updateSourceFile(
-// 	testSource!,
-// 	testSource!.statements.map((statement) => {
-// 		if(statement === varStatement) {
-// 			return newVarStatement;
-// 		}
-// 		return statement;
-// 	})
-// );
-
-
-// // console.log(updatedSourceFile);
-
-// updatedSourceFile.fileName = updatedSourceFile.fileName.replace(".ts", "-updated.ts");
-
-// const funcCalls: ts.CallExpression[] = [];
-// const findFuncCalls = (node: ts.Node) =>
-// {
-// 	if (ts.isCallExpression(node))
-// 	{
-// 		funcCalls.push(node);
-// 		return;
-// 	}
-
-// 	node.forEachChild(findFuncCalls);
-// }
-
-// findFuncCalls(updatedSourceFile);
-
-
-// const virtualFiles = new Map<string, ts.SourceFile>();
-// virtualFiles.set(updatedSourceFile.fileName, updatedSourceFile);
-
-// const compilerOptions: ts.CompilerOptions = {
-// 	// Your desired compiler options
-// 	target: ts.ScriptTarget.ESNext,
-// 	module: ts.ModuleKind.CommonJS,
-// };
-
-// console.log("CWD", process.cwd());
-
-// const customCompilerHost: ts.CompilerHost = {
-// 	getSourceFile: (fileName: string) =>
-// 	{
-// 		console.log("getSourceFile", fileName, virtualFiles.has(fileName));
-// 		return virtualFiles.get(fileName);
-// 	},
-// 	// Implement other methods as needed for your specific use case
-// 	writeFile: (fileName, data) => {
-// 		console.log("WRITING", data.toString());
-// 		fs.writeFileSync(fileName.replace(".ts", ".js"), data);
-// 	},
-// 	getDefaultLibFileName: () => 'lib.d.ts',
-// 	getCurrentDirectory: () => process.cwd(),
-// 	getCanonicalFileName: (fileName: string): string => fileName,
-// 	useCaseSensitiveFileNames: () => true,
-// 	getNewLine: (): string => '\n',
-// 	fileExists: (fileName: string): boolean =>
-// 	{
-// 		console.log("fileExists", fileName, virtualFiles.has(fileName));
-// 		return virtualFiles.has(fileName);
-// 	},
-// 	readFile: (fileName: string): string | undefined =>
-// 	{
-// 		console.log("NO SF");
-// 		const sf = virtualFiles.get(fileName);
-// 		console.log("NO SF");
-// 		if (sf)
-// 		{
-// 			console.log("A")
-// 			const sfContent = ts.createPrinter().printNode(ts.EmitHint.Unspecified, sf, sf);
-// 			console.log("B")
-// 			return sfContent;
-// 		}
-// 		return undefined;
-// 	},
-// 	// directoryExists: (directoryName: string): boolean =>
-// 	// {
-// 	// 	try
-// 	// 	{
-// 	// 		return fs.statSync(directoryName).isDirectory();
-// 	// 	} catch
-// 	// 	{
-// 	// 		return false;
-// 	// 	}
-// 	// },
-// 	// getDirectories: (directoryName: string): string[] =>
-// 	// {
-// 	// 	return fs.readdirSync(directoryName).filter(name => fs.statSync(path.join(directoryName, name)).isDirectory());
-
-// 	// },
-// 	// resolveModuleNames: (
-// 	// 	moduleNames: string[],
-// 	// 	containingFile: string,
-// 	// 	reusedNames?: string[],
-// 	// 	redirectedReferences?: ts.ResolvedProjectReference | undefined,
-// 	// 	options?
-// 	// ): (ts.ResolvedModule | undefined)[] =>
-// 	// {
-// 	// 	// Implement module resolution logic here
-// 	// 	return []
-// 	// },
-// 	// getDefaultLibLocation(): string
-// 	// {
-// 	// 	return 'lib.d.ts';
-// 	// }
-// };
-
-// console.log(1, "UPDATED FILENAME", updatedSourceFile.fileName)
-// const program2 = ts.createProgram([updatedSourceFile.fileName], compilerOptions, customCompilerHost);
-
-// console.log(updatedSourceFile.getChildCount())
-// console.log(2,
-// 	// virtualFiles.get(updatedSourceFile.fileName)
-// )
-// const emitResult = program2.emit(updatedSourceFile);
-
-// console.log(3)
-// console.log(emitResult);
-
-// if (emitResult.diagnostics.length > 0)
-// {
-// 	// Handle errors
-// 	console.error(ts.formatDiagnostics(emitResult.diagnostics, {
-// 		getCanonicalFileName: fileName => fileName,
-// 		getCurrentDirectory: () => '',
-// 		getNewLine: () => '\n'
-// 	}));
-// } else
-// {
-// 	console.log('Transpilation successful');
-// }
+if (emitResult.diagnostics.length > 0)
+{
+	// Handle errors
+	console.error(ts.formatDiagnostics(emitResult.diagnostics, {
+		getCanonicalFileName: fileName => fileName,
+		getCurrentDirectory: () => '',
+		getNewLine: () => '\n'
+	}));
+} else
+{
+	console.log('Transpilation successful');
+}
