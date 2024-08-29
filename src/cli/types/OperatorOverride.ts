d5832394c0e83f59159befcf8df73528f9285d19
diefbell <diefenbaker.bell@gmail.com>
2024-08-30 00:15:32 +0100

    Added lint script, fixed other issues

--- src/cli/types/OperatorOverride.ts
+++ src/cli/types/OperatorOverride.ts
@@ -1,6 +1,5 @@
-
 export type OperatorOverride = {
-	leftType: new (...args: unknown[]) => unknown;
-	rightType: new (...args: unknown[]) => unknown;
-	func: (a: unknown, b: unknown) => unknown;
+	leftType: new (...args: unknown[]) => unknown
+	rightType: new (...args: unknown[]) => unknown
+	func: (a: unknown, b: unknown) => unknown
 };
--- src/cli/types/Operators.ts
+++ src/cli/types/Operators.ts
@@ -3,7 +3,8 @@ export enum Operators
 	ADD = "ADD",
 	SUBTRACT = "SUBTRACT",
 	MULTIPLY = "MULTIPLY",
-	DIVIDE = "DIVIDE"
-}export type OperatorName = keyof typeof Operators;
-export const operators = Object.keys(Operators);
+	DIVIDE = "DIVIDE",
+}
 
+export type OperatorName = keyof typeof Operators;
+export const operators = Object.keys(Operators);
