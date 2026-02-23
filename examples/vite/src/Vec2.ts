e2c66a1d35880a9512e5ffc35f46ff7dfc1727b7
Dief Bell <diefenbaker.bell@gmail.com>
2026-02-23 15:01:25 +0000

    Updated NextJS and webpack examples

--- examples/vite/src/Vec2.ts
+++ examples/vite/src/Vec2.ts
@@ -4,9 +4,9 @@ export class Vec2 {
 		public y: number,
 	) {}
 
-	static readonly "+" = [
-		(a: Vec2, b: Vec2): Vec2 => new Vec2(a.x + b.x, a.y + b.y),
-	] as const;
+	static "+"(a: Vec2, b: Vec2): Vec2 {
+		return new Vec2(a.x + b.x, a.y + b.y);
+	}
 
 	toString(): string {
 		return `Vec2(${this.x}, ${this.y})`;
